8dae0421c663c25696399929bde1d91c0c1f39d2
Daniel de Paula e Silva <danielpes91@gmail.com>
2017-09-08 18:29:09 +0200

    Added font-awesome

---
+++ contents.js
@@ -0,0 +1,45 @@
+contents = [
+    {
+        title: "Experience",
+        type: "section",
+        items: [
+            {
+                inst: "École Polytechnique",
+                city: "Palaiseau",
+                country: "France",
+                role: "Big Data Engineer",
+                period: "Jul 2016 - Present",
+                duration: undefined,
+                description: "As part of the Data Science Initiative, I work in a project in partnership with the " +
+                    "French national health insurance fund (CNAMTS), being one of the main engineers implementing a " +
+                    "big data pipeline in Spark for processing over 3TBs of raw medical data into features for ML " +
+                    "models."
+            },
+            {
+                inst: "Icaro Tech",
+                city: "Campinas, SP",
+                country: "Brazil",
+                role: "R&D Engineer",
+                period: "Jan 2015 - Jun 2016",
+                duration: "1.5 year"
+            },
+            {
+                inst: "Icaro Tech",
+                city: "Campinas, SP",
+                country: "Brazil",
+                role: "Intern",
+                period: "Jul 2014 - Dec 2014",
+                duration: "1 year"
+            },
+            {
+                inst: "Lince",
+                city: "Sao Carlos, SP",
+                country: "Brazil",
+                role: "Web Development Intern",
+                period: "Jul 2016 - Present",
+                duration: undefined
+            },
+
+        ]
+    }
+]
