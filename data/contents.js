20ca8208a1cfb98b25e81dd493e29178ea5ddb82
Daniel de Paula e Silva <danielpes91@gmail.com>
2017-09-24 00:36:39 +0200

    Added vue and started adding "v-for"s

---
+++ data/contents.js
@@ -0,0 +1,107 @@
+var contents = {
+    "experiences": [
+        {
+            institution: "École Polytechnique",
+            city: "Palaiseau",
+            country: "France",
+            role: "Big Data Engineer",
+            period: "Jul 2016 - Present",
+            duration: undefined,
+            description: "As part of the Data Science Initiative, I work in a project in partnership with the " +
+                "French national health insurance fund (CNAMTS), being one of the main engineers implementing a " +
+                "big data pipeline in Spark for processing over 3TBs of raw medical data into features for ML " +
+                "models."
+        },
+        {
+            institution: "Icaro Tech",
+            city: "Campinas, SP",
+            country: "Brazil",
+            role: "R&D Engineer",
+            period: "Jan 2015 - Jun 2016",
+            duration: "1.5 year",
+            description: "At first, I worked with data analysis and visualization with R and SQL, then I started " +
+                "contributing to the company’s products development with code, management tasks and decision making."
+        },
+        {
+            institution: "Icaro Tech",
+            city: "Campinas, SP",
+            country: "Brazil",
+            role: "Intern",
+            period: "Jan 2014 - Dec 2014",
+            duration: "1 year",
+            description: "I started as a presales intern, working with price estimation and PoCs; then I changed to " +
+                "R&D to focus on data analysis and visualization."
+        },
+        {
+            institution: "Lince",
+            city: "Sao Carlos, SP",
+            country: "Brazil",
+            role: "Web Development Intern",
+            period: "Jan 2012 - Aug 2012",
+            duration: "8 months",
+            description: "I used Javascript, CSS and HTML to contribute to the creation of the tool WebNCL, a web " +
+                "player for NCL multimedia documents."
+        }
+
+    ],
+    "skills": [
+        {
+            "name": "Apache Spark",
+            "level": 8
+        },
+        {
+            "name": "Scala",
+            "level": 7
+        },
+        {
+            "name": "SQL",
+            "level": 7
+        },
+        {
+            "name": "Git",
+            "level": 6
+        },
+        {
+            "name": "Unix",
+            "level": 5
+        },
+        {
+            "name": "Web (front-end)",
+            "level": 5
+        },
+        {
+            "name": "Python",
+            "level": 4
+        },
+        {
+            "name": "Java",
+            "level": 4
+        },
+        {
+            "name": "Node.js",
+            "level": 4
+        },
+        {
+            "name": "Mach. Learning",
+            "level": 4
+        }
+    ],
+    "languages": [
+        {
+            "name": "Portuguese",
+            "level": 8
+        },
+        {
+            "name": "English",
+            "level": 7
+        },
+        {
+            "name": "French",
+            "level": 6
+        },
+        {
+            "name": "Spanish",
+            "level": 3
+        }
+    ]
+};
