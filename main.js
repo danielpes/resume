786a4b0de4cb5bbc9efe5242905950129b384c5b
Daniel de Paula e Silva <danielpes91@gmail.com>
2017-09-24 22:34:06 +0200

    Added navbar and improved vuejs usage
    Also, many visual changes and improved color palette

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
+var data = contents;
+
+data.info.age = calculateAge(new Date(1991, 3, 22));
+data.fonts = [
+    "Montserrat", "Lato", "Open Sans", "Merriweather"
+];
+data.currentFont = "Montserrat";
+data.baseFontSize = 13;
+
+
 var app = new Vue({
     el: '#page',
-    data: contents
+    data: data,
+    methods: {
+        print: window.print,
+        increaseFont: function () {
+            this.baseFontSize++;
+            $("html").css("font-size", this.baseFontSize)
+        },
+        decreaseFont: function () {
+            this.baseFontSize--;
+            $("html").css("font-size", this.baseFontSize)
+        }
+    }
 });
+
+function calculateAge (dateOfBirth) {
+    var now = new Date();
+    var age = now.getYear() - dateOfBirth.getYear();
+
+    var monthDiff = now.getMonth() - dateOfBirth.getMonth();
+    var dayDiff = now.getDate() < dateOfBirth.getDate();
+
+    if (monthDiff < 1 || (monthDiff === 0 && dayDiff < 1))
+        age = age - 1;
+
+    return age;
+}
