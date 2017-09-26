b10acaff4b24bdf2c01e7d6f05ee06dd96fa2928
Daniel de Paula e Silva <danielpes91@gmail.com>
2017-09-26 13:46:32 +0200

    Added top bar and some changes

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 var data = contents;
 
 data.info.age = calculateAge(new Date(1991, 3, 22));
+data.fonts = [
+    "Montserrat", "Lato", "Open Sans", "Merriweather"
+];
+data.currentFont = "Montserrat";
+data.baseFontSize = 13;
+
 
 var app = new Vue({
     el: '#page',
-    data: data
+    data: data,
+    methods: {
+        print: window.print,
+        increaseFont: function () {
+            this.baseFontSize++;
+            $("html").css("font-size", this.baseFontSize)
+        },
+        decreaseFont: function () {
+            this.baseFontSize--;
+            $("html").css("font-size", this.baseFontSize)
+        }
+    }
 });
 
 function calculateAge (dateOfBirth) {
